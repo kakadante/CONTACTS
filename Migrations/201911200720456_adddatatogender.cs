76bbdb273a432adc13b659ed34ae4379f4fb4798
KAKADANTE <danielmasila76@gmail.com>
2019-11-20 10:27:18 +0300

    All running well-code first population refused - manually added data to Sqlall gender table

---
+++ Migrations/201911200720456_adddatatogender.cs
@@ -0,0 +1,18 @@
+﻿namespace CONTACTS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class adddatatogender : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Genders", newName: "AllGenders");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.AllGenders", newName: "Genders");
+        }
+    }
+}
